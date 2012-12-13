49b1e3ac3695fd54100e1697df438f1a24842b13
mark neyer <mark@neyer.me>
2012-12-12 18:13:27 -0800

    its made\!

--- make-ssh-config.py
+++ make-ssh-config.py
@@ -3,10 +3,10 @@ import argparse
 
 
 
-ec2  = connect_to_region('us-east-1')
 
 def make_hosts_file(opts):
 
+    ec2  = connect_to_region(opts.region)
     instances = []
     reservations = ec2.get_all_instances()
     for r in reservations:
@@ -24,9 +24,10 @@ def make_hosts_file(opts):
 
 def main():
 
-    parser = argparse.ArgumentParser("make an ssh hosts file for")
+    parser = argparse.ArgumentParser("make an ssh hosts file for your all your pretty little ec2  instances. you can append the output to your .ssh config file if you want, in  ~/.ssh/config, but don't take my word for it!")
     parser.add_argument("--id-file-dir",default="~/.ssh",required=False)
     parser.add_argument("--user",default="ubuntu",required=False)
+    parser.add_argument("--region",default="eu-west-1",required=False)
     parser.add_argument("--extra-args",type=str,action='append',default=[],required=False)
     make_hosts_file(parser.parse_args())
 
